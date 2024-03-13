47242f294519f25d93f7141234092e8031d349c6
abubakkerdev <abubakkerdev@gmail.com>
2024-03-13 20:21:00 +0600

    day 02

--- js/app.js
+++ js/app.js
@@ -45,6 +45,27 @@ let supportListArr = [
     time: "4.00pm - 4.23pm",
     date: "March 12, 2024",
   },
+  {
+    uname: "Sahed Rahman",
+    batch: "FDR-2314",
+    topic: "Design",
+    time: "12.55pm - 1.05pm",
+    date: "March 13, 2024",
+  },
+  {
+    uname: "Kabir",
+    batch: "FDR-2403",
+    topic: "Design",
+    time: "1.15pm - 1.27pm",
+    date: "March 13, 2024",
+  },
+  {
+    uname: "Sifatul Islam",
+    batch: "MERN-2307",
+    topic: "Design",
+    time: "3.57pm - 4.05pm",
+    date: "March 13, 2024",
+  },
 ];
 
 let supportListData = "";
